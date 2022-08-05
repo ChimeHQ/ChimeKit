71a1882b45c165d13d5621a4c784eb142af9cfc1
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-08-05 15:40:25 -0400

    Removing AnyCodable

--- Package.swift
+++ Package.swift
@@ -10,13 +10,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ChimeHQ/ConcurrencyPlus", from: "0.2.3"),
-        .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.0"),
         .package(url: "https://github.com/ChimeHQ/ProcessEnv", from: "0.3.0"),
         .package(url: "https://github.com/ChimeHQ/LanguageClient", branch: "main"),
         .package(url: "https://github.com/ChimeHQ/LanguageServerProtocol", from: "0.7.3"),
     ],
     targets: [
-        .target(name: "ChimeKitWrapper", dependencies: ["ConcurrencyPlus", "AnyCodable", "ProcessEnv", "LanguageClient", "LanguageServerProtocol", "ChimeKit"]),
+        .target(name: "ChimeKitWrapper", dependencies: ["ConcurrencyPlus", "ProcessEnv", "LanguageClient", "LanguageServerProtocol", "ChimeKit"]),
         .binaryTarget(name: "ChimeKit", path: "ChimeKit.xcframework"),
     ]
 )
