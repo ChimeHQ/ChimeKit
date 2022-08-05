bc3b5bc9655953986cd6697826cf8846530d8543
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-08-05 12:20:59 -0400

    More deps

--- Package.swift
+++ Package.swift
@@ -10,6 +10,10 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ChimeHQ/ConcurrencyPlus", from: "0.2.3"),
+        .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.3"),
+        .package(url: "https://github.com/ChimeHQ/ProcessEnv", from: "0.3.0"),
+        .package(url: "https://github.com/ChimeHQ/LanguageClient.", from: "0.2.3"),
+        .package(url: "https://github.com/ChimeHQ/LanguageServerProtocol.", from: "0.7.3"),
     ],
     targets: [
         .target(name: "ChimeKitWrapper", dependencies: ["ConcurrencyPlus", "ChimeKit"]),
