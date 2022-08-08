e7e32fdf21ff9c19218abab2e82b26706a6e0749
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-08-08 11:59:07 -0400

    first pass at SPM-based framework

--- ChimeKit/Exports.swift
+++ ChimeKit/Exports.swift
@@ -1,2 +1,5 @@
+// These both need to be compiled with "-enable-library-evolution". However
+// that's only possible with unsafeFlags, which then makes them not work
+// as dependencies.
 @_exported import ChimeExtensionInterface
 @_exported import ChimeLSPAdapter
