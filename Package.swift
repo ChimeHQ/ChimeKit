890eaf957afb791917e10c4d54cc27b34289ecef
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-08-08 13:02:09 -0400

    Public dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .package(url: "https://github.com/ChimeHQ/ProcessEnv", from: "0.3.0"),
         .package(url: "https://github.com/ChimeHQ/LanguageClient", branch: "main"),
         .package(url: "https://github.com/ChimeHQ/LanguageServerProtocol", from: "0.7.3"),
-        .package(name: "ProcessServiceClient", path: "/Users/matt/Chime/Code/ProcessService"),
+        .package(url: "https://github.com/ChimeHQ/ProcessService", branch: "main"),
     ],
     targets: [
         .target(name: "ChimeExtensionInterface",
@@ -26,7 +26,7 @@ let package = Package(
                                "ChimeExtensionInterface",
                                "ConcurrencyPlus",
                                "ProcessEnv",
-                               "ProcessServiceClient"]),
+                               .product(name: "ProcessServiceClient", package: "ProcessService")]),
         .binaryTarget(name: "ChimeKit",
                       path: "ChimeKit.xcframework"),
         .target(name: "ChimeKitWrapper",
