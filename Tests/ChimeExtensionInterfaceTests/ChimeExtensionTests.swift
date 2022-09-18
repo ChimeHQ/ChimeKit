aa628392f77bacea395951809ecc52b1a3d9fb8b
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-09-18 18:22:25 -0400

    Really simple non-ui test

--- Tests/ChimeExtensionInterfaceTests/ChimeExtensionTests.swift
+++ Tests/ChimeExtensionInterfaceTests/ChimeExtensionTests.swift
@@ -5,12 +5,44 @@ import XCTest
 final class ExampleNonUIExtension: ChimeExtension {
 	func acceptHostConnection(_ host: HostProtocol) throws {
 	}
-
-	var scene: some NewChimeExtensionScene { return UnsupportedScene.all }
 }
 
-final class ChimeExtensionTests: XCTestCase {
-	func testNonUIExtension() {
+final class MockHost: HostProtocol {
+    func textContent(for documentId: DocumentIdentity, in range: ChimeExtensionInterface.TextRange) async throws -> CombinedTextContent {
+        let posA = LineRelativeTextPosition(line: 0, offset: 0)
+        let posB = LineRelativeTextPosition(line: 0, offset: 0)
+        let relativeRange = posA..<posB
+        let combinedRange = CombinedTextRange(version: 1,
+                                              range: NSRange(location: 0, length: 0),
+                                              lineRelativeRange: relativeRange,
+                                              limit: 0)
+        return CombinedTextContent(string: "", range: combinedRange)
+    }
 
+    func textContent(for documentId: DocumentIdentity) async throws -> (String, Int) {
+        return ("", 1)
+    }
+
+    func textBounds(for documentId: DocumentIdentity, in ranges: [ChimeExtensionInterface.TextRange], version: Int) async throws -> [NSRect] {
+        return []
+    }
+
+    func publishDiagnostics(_ diagnostics: [Diagnostic], for documentURL: URL, version: Int?) {
+    }
+
+    func invalidateTokens(for documentId: DocumentIdentity, in target: TextTarget) {
+    }
+
+    func documentServiceConfigurationChanged(for documentId: DocumentIdentity, to configuration: ServiceConfiguration) {
+    }
+}
+
+@available(macOS 13.0, *)
+final class ChimeExtensionTests: XCTestCase {
+	func testNonUIExtension() throws {
+        let ext = ExampleNonUIExtension()
+        let host = MockHost()
+
+        XCTAssertNoThrow(try ext.acceptHostConnection(host))
 	}
 }
