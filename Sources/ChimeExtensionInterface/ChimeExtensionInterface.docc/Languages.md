e552fa97ac2e2353adaa910859482823fffbced2
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-10-04 09:04:48 -0400

    Migrating in more language UTI definitions

--- Sources/ChimeExtensionInterface/ChimeExtensionInterface.docc/Languages.md
+++ Sources/ChimeExtensionInterface/ChimeExtensionInterface.docc/Languages.md
@@ -11,5 +11,6 @@ If Chime does not support a language you'd like to use, the first step is to let
 - Create an SPM-based [tree-sitter](https://github.com/ChimeHQ/SwiftTreeSitter#language-parsers) parser
 - Ensure that parser has the needed queries defined
 - Encorporate the parser and queries into a new Chime build
+- Define Uniform Type Identifiers for the language
 
 Your process will depend on what kind of support you need to build. If you are using an LSP server, at a minimum, you'll need to ensure that [LanguageServerProtocol](https://github.com/ChimeHQ/LanguageServerProtocol) has a language identifier constant defined. After that, your extension does the rest, most likely by making use of the `LSPService` API within the `ChimeLSPAdapter` module of ChimeKit.
---
+++ Sources/ChimeExtensionInterface/UTType+Filename.swift
@@ -0,0 +1,28 @@
+import Foundation
+import UniformTypeIdentifiers
+
+public extension UTType {
+	/// Resolves a UTI by considering both an identifier name and the file's URL
+	///
+	/// This is needed for things like "Makefile", which do not use any of the standard
+	/// UTI mechanisms for recognizing a file's type.
+	static func resolveTypeName(_ name: String, url: URL) -> String {
+		switch url.lastPathComponent.lowercased() {
+		case "gemfile", "fastfile", "podfile", "rakefile":
+			return UTType.rubyScript.identifier
+		default:
+			break
+		}
+
+		return name
+	}
+	
+	/// Resolves a UTType by considering both an identifier name and the file's URL
+	///
+	/// See `resolveTypeName(_:, url:)`
+	static func resolveType(with name: String, url: URL) -> UTType? {
+		let typeName = resolveTypeName(name, url: url)
+
+		return UTType(typeName)
+	}
+}
