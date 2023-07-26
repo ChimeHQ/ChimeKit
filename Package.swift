daee6ea4969a13535fd3d66f8ebc0fe7d28232fc
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-07-26 06:34:36 -0400

    Explictly require ProcessEnv 0.3.1

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/ChimeHQ/AsyncXPCConnection", from: "1.0.0"),
 		.package(url: "https://github.com/ChimeHQ/Extendable", from: "0.1.1"),
-		.package(url: "https://github.com/ChimeHQ/ProcessEnv", from: "0.3.0"),
+		.package(url: "https://github.com/ChimeHQ/ProcessEnv", from: "0.3.1"),
 		.package(url: "https://github.com/ChimeHQ/LanguageClient", from: "0.6.0"),
 		.package(url: "https://github.com/ChimeHQ/LanguageServerProtocol", from: "0.10.0"),
 		.package(url: "https://github.com/mattmassicotte/Queue", from: "0.1.4"),
